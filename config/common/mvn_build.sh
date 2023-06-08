b509b37b2721963db55117cfd5631a4554945ede
Bob Beck <bob_beck@sil.org>
2023-06-08 18:06:44 +0000

    removed sudo

--- config/common/mvn_build.sh
+++ config/common/mvn_build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export NODE_OPTIONS=--openssl-legacy-provider
-sudo mvn clean install
+mvn clean install
 
 # add "react-scripts --openssl-legacy-provider build/start to
 # package.json in quack/ui/src
