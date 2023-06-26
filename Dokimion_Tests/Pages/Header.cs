5ceee29fb6bb7128a60b6db4acaccbabbba97114
Roopa <rupa_salagala@sil.org>
2023-06-26 12:11:09 -0400

    Checked in runsettings for Dokimion_Tests

---
+++ Dokimion_Tests/Pages/Header.cs
@@ -0,0 +1,48 @@
+﻿using Boa.Constrictor.Selenium;
+using OpenQA.Selenium;
+using static Boa.Constrictor.Selenium.WebLocator;
+
+
+
+namespace Dokimion.Pages
+{
+    public class Header
+    {
+        public static IWebLocator UserInfo => L(
+                    "Display User Name",
+                    By.XPath("//a[@id = 'bd-login']"));
+
+        public static IWebLocator ProfileLink => L("Profile Link", By.XPath("//a[contains(text(),'Profile')]"));
+
+        public static IWebLocator LogoutLink => L("Logout Link", By.XPath("//a[contains(text(),'Log out')]"));
+
+        public static IWebLocator ProfileName => L("Profile Name", By.XPath("//h1[contains(text(),'Tester')]"));
+
+
+        public static IWebLocator ProjectsLink => L("Project Link", By.XPath("//*[@id='bd-projects']"));
+
+
+        public static IWebLocator AllLink => L("All Link", By.XPath("//a[contains(text(),'All')]"));
+
+        public static IWebLocator Dokimion => L("Dokimion", By.XPath("//a[contains(text(),'Dokimion')]"));
+
+    }
+
+
+
+    //public string xpathProfileLink = "//a[contains(text(),'Profile')]";
+
+
+
+
+    //public string xpathProjectLink = "//*[@id='bd-projects']";
+
+    //public string xpathAllLink = "//a[contains(text(),'All')]";
+
+    //public string xpathTestManager = "//a[contains(text(),'Test Manager')]";
+
+    //public string xpathLogout = "//a[contains(text(),'Log out')]";
+
+
+
+}
---
+++ Dokimion_Tests/Tests/HeaderUserAccessTests.cs
@@ -0,0 +1,152 @@
+﻿using Dokimion.Interactions;
+using Dokimion.Pages;
+using Boa.Constrictor.Screenplay;
+using Boa.Constrictor.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Dokimion.Tests
+{
+    internal class HeaderUserAccessTests
+    {
+        private IActor Actor;
+        UserActions userActions;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+
+            userActions = new UserActions();
+            userActions.LogConsoleMessage("In one time Set up :" + TestContext.CurrentContext.Test.ClassName);
+            userActions.LogConsoleMessage("Register Driver & Open the QuAck website");
+
+            Actor = new Actor(name: userActions.ActorName, logger: new NoOpLogger());
+            Actor.Can(BrowseTheWeb.With(new ChromeDriver(userActions.GetChromeOptions())));
+            Actor.AttemptsTo(Navigate.ToUrl(userActions.QuackUrl));
+
+        }
+        [TearDown]
+        public void TearDownAfterTestCase()
+        {
+            userActions.TearDownAfterTestcase();
+        }
+
+
+        [OneTimeTearDown]
+        public void QuitBrowser()
+        {
+            Actor.AttemptsTo(QuitWebDriver.ForBrowser());
+        }
+
+        [Test]
+        public void TC3UserOptions()
+        {
+            userActions.LogConsoleMessage(TestContext.CurrentContext.Test.MethodName!);
+            userActions.LogConsoleMessage("Set Up : ");
+            userActions.LogConsoleMessage("Login as User");
+            Actor.AttemptsTo(LoginUser.For(userActions.Username!, userActions.Password!));
+
+            try
+            {
+                userActions.LogConsoleMessage("Action steps : ");
+                userActions.LogConsoleMessage("Click on left side on User link");
+                Actor.AttemptsTo(Click.On(Header.UserInfo));
+
+                userActions.LogConsoleMessage("Verify : Profile and Logout links displayed");
+                bool profileExists = Actor.AskingFor(Existence.Of(Header.ProfileLink));
+                bool logoutExists = Actor.AskingFor(Existence.Of(Header.LogoutLink));
+
+                Assert.That((profileExists && logoutExists), Is.True);
+                
+            }
+            finally
+            {
+                userActions.LogConsoleMessage("Clean up : Logout User");
+                Actor.AttemptsTo(Refresh.Browser());
+                Actor.AttemptsTo(Logout.For());
+            }
+        }
+
+
+        [Test]
+        public void TC4UserClickOnProfile()
+        {
+            userActions.LogConsoleMessage(TestContext.CurrentContext.Test.MethodName!);
+            userActions.LogConsoleMessage("Set Up : ");
+            userActions.LogConsoleMessage("Login as User");
+            Actor.AttemptsTo(LoginUser.For(userActions.Username!, userActions.Password!));
+
+            try
+            {
+                userActions.LogConsoleMessage("Action steps : ");
+                userActions.LogConsoleMessage("Click on left side on User link");
+                Actor.AttemptsTo(Click.On(Header.UserInfo));
+
+                userActions.LogConsoleMessage("Click on Profile link");
+                Actor.AttemptsTo(Click.On(Header.ProfileLink));
+
+                userActions.LogConsoleMessage("Verify : User name is displayed");
+                Actor.WaitsUntil(Text.Of(Header.ProfileName), ContainsSubstring.Text(userActions.DisplayUserName));
+            }
+            finally
+            {
+                userActions.LogConsoleMessage("Clean up : Logout User");
+                Actor.AttemptsTo(Refresh.Browser());
+                Actor.AttemptsTo(Logout.For());
+            }
+        }
+
+
+        [Test]
+        public void TC5UserClickOnProjects()
+        {
+            userActions.LogConsoleMessage(TestContext.CurrentContext.Test.MethodName!);
+            userActions.LogConsoleMessage("Set Up : ");
+            userActions.LogConsoleMessage("Login as User");
+            Actor.AttemptsTo(LoginUser.For(userActions.Username!, userActions.Password!));
+
+            try
+            {
+                userActions.LogConsoleMessage("Action steps : ");
+                userActions.LogConsoleMessage("Click on right side Projects link");
+                Actor.AttemptsTo(Click.On(Header.ProjectsLink));
+
+                //permission granted only for this project for user (Tester & User : Roopa)
+                userActions.LogConsoleMessage("Verify : All and Test Manager project links are in dropdown");
+                bool allExists = Actor.AskingFor(Appearance.Of(Header.AllLink));
+                bool dokimionExists = Actor.AskingFor(Appearance.Of(Header.Dokimion));
+                Assert.That((allExists && dokimionExists), Is.True);
+            }
+            finally
+            {
+                userActions.LogConsoleMessage("Clean up : Logout User");
+                Actor.AttemptsTo(Refresh.Browser());
+                Actor.AttemptsTo(Logout.For());
+            }
+        }
+
+
+        [Test]
+        public void TC6UserClickOnLogout()
+        {
+            userActions.LogConsoleMessage(TestContext.CurrentContext.Test.MethodName!);
+            userActions.LogConsoleMessage("Set Up : ");
+            userActions.LogConsoleMessage("Login as User");
+            Actor.AttemptsTo(LoginUser.For(userActions.Username!, userActions.Password!));
+
+
+            userActions.LogConsoleMessage("Action steps : ");
+            userActions.LogConsoleMessage("Click on left side on User link");
+            Actor.AttemptsTo(Click.On(Header.UserInfo));
+
+            userActions.LogConsoleMessage("Click on Logout link");
+            Actor.AttemptsTo(Click.On(Header.LogoutLink));
+
+            userActions.LogConsoleMessage("Verify : User logged out");
+            Actor.WaitsUntil(Text.Of(LoginPage.LoginPageWelcomeMsg), IsEqualTo.Value("Please sign in"));
+
+            userActions.LogConsoleMessage("Clean up :");
+
+        }
+
+    }
+}
