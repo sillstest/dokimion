2a48095181e0f153e77a52e3c54366c0ad3fe7a2
Bob Beck <bob_beck@sil.org>
2023-07-06 15:40:47 +0000

    bug fixes

--- tools/get_test_results.py
+++ tools/get_test_results.py
@@ -8,6 +8,7 @@
 import os, fnmatch
 import sys
 import glob
+import shutil
 
 def locate(pattern, root=os.curdir):
    for path, dirs, files in os.walk(os.path.abspath(root)):
@@ -20,22 +21,32 @@ def main():
       print("Unsupported number of arguments: ", len(sys.argv));
       sys.exit(-1);
 
+   quackAbsHomeDir = os.path.join(os.path.dirname(__file__), "../");
    testResultsDir = sys.argv[1];
 
-   for f in glob.glob(os.path.join(testResultsDir, "TEST-*.xml")):
+   # delete all test result files in test results dir
+   testResultsFileNamePattern = "TEST-*.xml";
+   for f in glob.glob(os.path.join(quackAbsHomeDir, testResultsDir, testResultsFileNamePattern)):
       os.remove(f);
 
-   for xmlFile in locate("TEST-*.xml"):
+   # copy all surefire test results file to test results dir
+   for xmlFile in locate(testResultsFileNamePattern):
       try:
-         filename = os.path.basename(xmlFile);
-         with open(xmlFile, 'r') as fin:
-            sys.stdout = open(os.path.join(testResultsDir, filename), "w+")
-            print(fin.read());
-            fin.close();
+         srcXmlFileName = os.path.basename(xmlFile);
+         destXmlFileName = os.path.join(quackAbsHomeDir, testResultsDir, srcXmlFileName);
+
+         shutil.copy(xmlFile, destXmlFileName);
 
       except:
           print (xmlFile, "File write error");
 
+
+   # save new test results files to test results dir
+   os.chdir(os.path.join(quackAbsHomeDir, testResultsDir));
+   os.system("/usr/bin/git add *");
+   os.system("/usr/bin/git commit -m \"new test results files\"");
+   os.system("/usr/bin/git pull");
+   os.system("/usr/bin/git push");
 
 if __name__ == "__main__":
    main()
