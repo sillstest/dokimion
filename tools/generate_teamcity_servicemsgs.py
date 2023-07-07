8d3cf6f05d4195b6fa171ba5776a180fc532727c
Bob Beck <bob_beck@sil.org>
2023-07-07 15:13:31 +0000

    file name and purpose change

---
+++ tools/generate_teamcity_servicemsgs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+#
+# Utility to copy surefire test results files to stdout
+# so the files can be parsed by team city's XML report parsing
+# build feature
+#
+
+import os, fnmatch
+import sys
+import glob
+
+def main():
+
+   # find all test results xml files
+   for xmlFile in glob.glob("**/TEST-*.xml" + testResultsFileNamePattern, recursive=True):
+      print("##teamcity[importData type='surefire' path='xmlFile' parseOutOfDate='true']");
+
+
+
+if __name__ == "__main__":
+   main()
+
+
+
+
+
