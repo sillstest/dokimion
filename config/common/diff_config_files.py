6bea74722a20525f90bff2b438337b90363c4be9
Bob Beck <bob_beck@sil.org>
2023-08-22 17:21:29 +0000

    quack to dokimion name change

--- config/common/diff_config_files.py
+++ config/common/diff_config_files.py
@@ -7,59 +7,70 @@ import os
 import platform
 
 VM_common_configFiles = [
-   [ "quack/config/production/VM_common"                 ],
+   [ "dokimion/config/production/common"                 ],
    [ "rsyslog.conf",          "/etc"                     ],
    [ "rsyslog.service",       "/etc/systemd/system/multi-user.target.wants" ],
    [ "nginx.conf",            "/etc/nginx"               ]
 ]
 
 VM_dokimion_configFiles = [
-   [ "dokimion/config/production/dokimion"                  ],
+   [ "dokimion/config/production/dokimion"               ],
    [ "load_balancer.conf",    "/etc/nginx/conf.d"        ]
 ]
 
-VM_quack1_configFiles = [
-   [ "quack/config/production/VM_quack1"                 ],
-   [ "25-quack.conf",         "/etc/rsyslog.d"           ],
-   [ "mongod1.conf",          "/etc"                     ],
-   [ "mongod1.service",       "/etc/systemd/system"      ],
-   [ "quack.properties",      "/etc/quack"               ],
-   [ "quack1.conf",           "/etc/nginx/sites-enabled" ],
-   [ "quack1_server.service", "/etc/systemd/system"      ],
-   [ "quack1_ui.service",     "/etc/systemd/system"      ]
+VM_dokimion1_configFiles = [
+   [ "dokimion/config/production/dokimion1"                 ],
+   [ "25-dokimion.conf",         "/etc/rsyslog.d"           ],
+   [ "mongod1.conf",             "/etc"                     ],
+   [ "mongod1.service",          "/etc/systemd/system"      ],
+   [ "quack.properties",         "/etc/dokimion"            ],
+   [ "dokimion1.conf",           "/etc/nginx/sites-enabled" ],
+   [ "dokimion1_server.service", "/etc/systemd/system"      ],
+   [ "dokimion1_ui.service",     "/etc/systemd/system"      ]
 ]
 
-VM_quack2_configFiles = [
-   [ "quack/config/production/VM_quack2"                 ],
-   [ "25-quack.conf",         "/etc/rsyslog.d"           ],
-   [ "mongod2.conf",          "/etc"                     ],
-   [ "mongod2.service",       "/etc/systemd/system"      ],
-   [ "quack.properties",      "/etc/quack"               ],
-   [ "quack2.conf",           "/etc/nginx/sites-enabled" ],
-   [ "quack2_server.service", "/etc/systemd/system"      ],
-   [ "quack2_ui.service",     "/etc/systemd/system"      ]
+VM_dokimion2_configFiles = [
+   [ "dokimion/config/production/dokimion2"                 ],
+   [ "25-dokimion.conf",         "/etc/rsyslog.d"           ],
+   [ "dokimion2.conf",           "/etc"                     ],
+   [ "dokimion2.service",        "/etc/systemd/system"      ],
+   [ "quack.properties",         "/etc/dokimion"            ],
+   [ "dokimion2.conf",           "/etc/nginx/sites-enabled" ],
+   [ "dokimion2_server.service", "/etc/systemd/system"      ],
+   [ "dokimion2_ui.service",     "/etc/systemd/system"      ]
 ]
 
-VM_quack3_configFiles = [
-   [ "quack/config/production/VM_quack3"                 ],
-   [ "25-quack.conf",         "/etc/rsyslog.d"           ],
-   [ "mongod3.conf",          "/etc"                     ],
-   [ "mongod3.service",       "/etc/systemd/system"      ],
-   [ "quack.properties",      "/etc/quack"               ],
-   [ "quack3.conf",           "/etc/nginx/sites-enabled" ],
-   [ "quack3_server.service", "/etc/systemd/system"      ],
-   [ "quack3_ui.service",     "/etc/systemd/system"      ]
+VM_dokimion3_configFiles = [
+   [ "dokimion/config/production/dokimion3"                 ],
+   [ "25-dokimion3.conf",        "/etc/rsyslog.d"           ],
+   [ "mongod3.conf",             "/etc"                     ],
+   [ "mongod3.service",          "/etc/systemd/system"      ],
+   [ "quack.properties",         "/etc/dokimion"            ],
+   [ "dokimion3.conf",           "/etc/nginx/sites-enabled" ],
+   [ "dokimion3_server.service", "/etc/systemd/system"      ],
+   [ "dokimion3_ui.service",     "/etc/systemd/system"      ]
 ]
 
-VM_quack4_configFiles = [
-   [ "quack/config/production/VM_quack4"                 ],
-   [ "25-quack.conf",         "/etc/rsyslog.d"           ],
-   [ "mongod4.conf",          "/etc"                     ],
-   [ "mongod4.service",       "/etc/systemd/system"      ],
-   [ "quack.properties",      "/etc/quack"               ],
-   [ "quack4.conf",           "/etc/nginx/sites-enabled" ],
-   [ "quack4_server.service", "/etc/systemd/system"      ],
-   [ "quack4_ui.service",     "/etc/systemd/system"      ]
+VM_dokimion4_configFiles = [
+   [ "dokimion/config/production/dokimion4"                 ],
+   [ "25-dokimion4.conf",        "/etc/rsyslog.d"           ],
+   [ "mongod4.conf",             "/etc"                     ],
+   [ "mongod4.service",          "/etc/systemd/system"      ],
+   [ "quack.properties",         "/etc/dokimion"            ],
+   [ "dokimion4.conf",           "/etc/nginx/sites-enabled" ],
+   [ "dokimion4_server.service", "/etc/systemd/system"      ],
+   [ "dokimion4_ui.service",     "/etc/systemd/system"      ]
+]
+
+VM_dev_configFiles = [
+   [ "dokimion/config/common/laptop_VM"                 ],
+   [ "25-dokimion_dev.conf",        "/etc/rsyslog.d"           ],
+   [ "mongod_dev.conf",             "/etc"                     ],
+   [ "mongod_dev.service",          "/etc/systemd/system"      ],
+   [ "quack.properties",            "/etc/dokimion"            ],
+   [ "dokimion_dev.conf",           "/etc/nginx/sites-enabled" ],
+   [ "dokimion_server_dev.service", "/etc/systemd/system"      ],
+   [ "dokimion_ui_dev.service",     "/etc/systemd/system"      ]
 ]
 
 def main():
@@ -78,68 +89,71 @@ def main():
 
     hostname = platform.node()
 
-    # diff quack files
+    # diff dokimion files
     if "dokimion" == hostname:
-       midDir = VM_quack_configFiles[0][0]
+       midDir = VM_dokimion_configFiles[0][0]
 
-       for i in range(len(VM_quack_configFiles)-1):
+       for i in range(len(VM_dokimion_configFiles)-1):
            if i == 0: continue
-           configFile = VM_quack_configFiles[i][0]
-           destDir = VM_quack_configFiles[i][1]
+           configFile = VM_dokimion_configFiles[i][0]
+           destDir = VM_dokimion_configFiles[i][1]
            print( "\'diff " + configFile + "\'" );
            print("----------------------------");
            os.system("/usr/bin/diff " + os.path.join( rootDir, midDir, configFile ) + " " +  destDir );
 
-    # diff quack1 files
-    elif "quack1" == hostname:
-       midDir = VM_quack1_configFiles[0][0]
+    # diff dokimion1 files
+    elif "dokimion1" == hostname:
+       midDir = VM_dokimion1_configFiles[0][0]
 
-       for i in range(len(VM_quack1_configFiles)-1):
+       for i in range(len(VM_dokimion1_configFiles)-1):
            if i == 0: continue
-           configFile = VM_quack1_configFiles[i][0]
-           destDir = VM_quack1_configFiles[i][1]
+           configFile = VM_dokimion1_configFiles[i][0]
+           destDir = VM_dokimion1_configFiles[i][1]
            print( "\'diff " + configFile + "\'" );
            print("----------------------------");
            os.system("/usr/bin/diff " + os.path.join( rootDir, midDir, configFile ) + " " +  destDir );
 
-    # diff quack2 files
-    elif "quack2" == hostname:
-       midDir = VM_quack2_configFiles[0][0]
+    # diff dokimion2 files
+    elif "dokimion2" == hostname:
+       midDir = VM_dokimion2_configFiles[0][0]
 
-       for i in range(len(VM_quack2_configFiles)-1):
+       for i in range(len(VM_dokimion2_configFiles)-1):
            if i == 0: continue
-           configFile = VM_quack2_configFiles[i][0]
-           destDir = VM_quack2_configFiles[i][1]
+           configFile = VM_dokimion2_configFiles[i][0]
+           destDir = VM_dokimion2_configFiles[i][1]
            print( "\'diff " + configFile + "\'" );
            print("----------------------------");
            os.system("/usr/bin/diff " + os.path.join( rootDir, midDir, configFile ) + " " +  destDir );
 
-    # diff quack3 files
-    elif "quack3" == hostname:
-       midDir = VM_quack3_configFiles[0][0]
+    # diff dokimion3 files
+    elif "dokimion3" == hostname:
+       midDir = VM_dokimion3_configFiles[0][0]
 
-       for i in range(len(VM_quack3_configFiles)-1):
+       for i in range(len(VM_dokimion3_configFiles)-1):
            if i == 0: continue
-           configFile = VM_quack3_configFiles[i][0]
-           destDir = VM_quack3_configFiles[i][1]
+           configFile = VM_dokimion3_configFiles[i][0]
+           destDir = VM_dokimion3_configFiles[i][1]
            print( "\'diff " + configFile + "\'" );
            print("----------------------------");
            os.system("/usr/bin/diff " + os.path.join( rootDir, midDir, configFile ) + " " +  destDir );
 
-    # diff quack4 files
-    elif "quack4" == hostname:
-       midDir = VM_quack4_configFiles[0][0]
+    # diff dokimion4 files
+    elif "dokimion4" == hostname:
+       midDir = VM_dokimion4_configFiles[0][0]
 
-       for i in range(len(VM_quack4_configFiles)-1):
+       for i in range(len(VM_dokimion4_configFiles)-1):
            if i == 0: continue
-           configFile = VM_quack4_configFiles[i][0]
-           destDir = VM_quack4_configFiles[i][1]
+           configFile = VM_dokimion4_configFiles[i][0]
+           destDir = VM_dokimion4_configFiles[i][1]
            print( "\'diff " + configFile + "\'" );
            print("----------------------------");
            os.system("/usr/bin/diff " + os.path.join( rootDir, midDir, configFile ) + " " +  destDir );
 
 
     return
+
+   elif "quackproto" == hostname:
+
 
 
 
