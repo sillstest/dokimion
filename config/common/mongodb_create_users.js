87015441ec1c4cafb364a08171f0c5e67b1ad63f
Bob Beck <bob_beck@sil.org>
2023-08-24 10:53:30 -0400

    new script

---
+++ config/common/mongodb_create_users.js
@@ -0,0 +1,25 @@
+//use admin
+db = db.getSiblingDB('admin')
+
+db.createUser(
+{
+   user: "admin",
+   pwd: passwordPrompt(),
+   roles: [
+      { role: "userAdminAnyDatabase", db: "admin" },
+      { role: "readWriteAnyDatabase", db: "admin" }
+   ]
+})
+
+//use test
+db = db.getSiblingDB('test')
+
+db.createUser(
+{
+   user: "dokimion",
+   pwd: "$2b$12$XouFVDvizUZOgqkKQlwG2.4.Cmdc1025smEd5rOGoUWDC3IzZiB1q",
+   roles: [
+      { role: "userAdmin", db: "dokimion" },
+      { role: "readWrite", db: "dokimion" }
+   ]
+})
