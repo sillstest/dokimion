586e4ef4c18356a990021d45bd5aaf3102df1861
Bob Beck <bob_beck@sil.org>
2023-12-21 12:42:36 -0500

    changed service file names

--- config/common/deploy.sh
+++ config/common/deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -x
 # assume in quack repo - root dir
 
-# prepare files for move to /deploy
 pushd ui
 tar cvzf ui.tgz src/
 popd
 
-# move files from /deploy to 
+# move files 
 pushd /home/dokimion
 rm -rf dokimion
 mkdir dokimion
@@ -18,16 +17,8 @@ cp ~1/assembly/target/lib/jetty-runner.jar .
 tar xvzf ui.tgz
 
 # restart relevant system services
-if [ $1 != "_dev" ]
-then
-  systemctl restart dokimion$1_server.service
-  systemctl restart dokimion$1_ui.service
-  systemctl status dokimion$1_server.service
-  systemctl status dokimion$1_ui.service
-else
-  systemctl restart dokimion_server_dev.service
-  systemctl restart dokimion_ui_dev.service
-  systemctl status dokimion_server_dev.service
-  systemctl status dokimion_ui_dev.service
-fi
+systemctl restart dokimion$1_server.service
+systemctl restart dokimion$1_ui.service
+systemctl status dokimion$1_server.service
+systemctl status dokimion$1_ui.service
 
