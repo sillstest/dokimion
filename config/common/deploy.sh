0889bcffde5aaa5cee5b697e7e69c1c6f8ef19fd
Bob Beck <bob_beck@sil.org>
2024-01-19 10:45:47 -0500

    more fixes

--- config/common/deploy.sh
+++ config/common/deploy.sh
@@ -17,6 +17,9 @@ cp ~1/assembly/target/lib/jetty-runner.jar .
 cp ~1/tools/aes.java .
 cp ~1/tools/RoleCapability.java .
 cp ~1/tools/AdminPassword.java .
+cp ~1/tools/TestcaseSizes.java .
+cp ~1/tools/mongodb_RoleCapability_init.js .
+cp ~1/tools/mongodb_TestcaseSizes_init.js .
 
 tar xvzf ui.tgz
 
