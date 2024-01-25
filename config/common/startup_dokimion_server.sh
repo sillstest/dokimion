78b184141049f2ac433e1a500f3c611a4778a91d
Bob Beck <bob_beck@sil.org>
2024-01-25 09:24:29 -0500

    added defaut project attributes collection init

--- config/common/startup_dokimion_server.sh
+++ config/common/startup_dokimion_server.sh
@@ -3,10 +3,12 @@
 # $1 - SERVER_NO
 
 cd /home/dokimion/dokimion
-/usr/bin/javac RoleCapability.java aes.java
+/usr/bin/javac RoleCapability.java aes.java AdminPassword.java
 /usr/bin/java RoleCapability
 /usr/bin/javac TestcaseSizes.java aes.java AdminPassword.java
 /usr/bin/java TestcaseSizes
+/usr/bin/javac DefaultProjectAttributes.java aes.java AdminPassword.java
+/usr/bin/java DefaultProjectAttributes
 
 /usr/bin/java -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/dokimion/dokimion/jetty-runner.jar /home/dokimion/dokimion/dokimion.war | awk -v SERVER_NO=$1 '{printf "dokimion_server_" SERVER_NO; print $0}' | logger -p user.info
 
