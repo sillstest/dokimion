11ce35433b0c532ca477c468d9728c71e0a4bbb7
bob <bob_beck@sil.org>
2024-03-28 11:55:20 -0400

    bug fix

--- config/teamcity/package_artifacts.sh
+++ config/teamcity/package_artifacts.sh
@@ -12,9 +12,9 @@ cp ../tools/DefaultProjectAttributes.java .
 cp ../tools/mongodb_RoleCapability_init.js .
 cp ../tools/mongodb_TestcaseSizes_init.js .
 cp ../tools/mongodb_DefaultProjectAttributes_init.js .
+tar xvzf ui.tgz
 cp ../config/common/startup_dokimion_server.sh .
 cp ../config/common/startup_dokimion_ui.sh .
-/bin/tar cvzf tools.tgz aes.java RoleCapability.java AdminPassword.java TestcaseSizes.java DefautlProjectAttributes.java mongodb_RoleCapability_init.js mongodb_TestcaseSizes_init.js mongodb_DefaultProjectAttributes_init.js startup_dokimion_server.sh startup_dokimion_ui.sh
 chmod -R a+w *
 chmod -R a+w .
 cd src
