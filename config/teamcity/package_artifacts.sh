6f1a53ba9d15e8712938b2e1263d497898c78439
bob <bob_beck@sil.org>
2024-03-28 10:54:50 -0400

    new script

---
+++ config/teamcity/package_artifacts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+pwd
+cd ../
+cd ui
+pwd
+/bin/tar cvzf ui.tgz src/
+cp ../tools/aes.java .
+cp ../tools/RoleCapability.java .
+cp ../tools/AdminPassword.java .
+cp ../tools/TestcaseSizes.java .
+cp ../tools/DefaultProjectAttributes.java .
+cp ../tools/mongodb_RoleCapability_init.js .
+cp ../tools/mongodb_TestcaseSizes_init.js .
+cp ../tools/mongodb_DefaultProjectAttributes_init.js .
+cp ../config/common/startup_dokimion_server.sh .
+cp ../config/common/startup_dokimion_ui.sh .
+chmod -R a+w *
+chmod -R a+w .
+cd src
+cp -r node_modules/tinymce* .
+
+
