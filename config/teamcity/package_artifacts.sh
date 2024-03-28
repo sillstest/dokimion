aa285493e49f769c7c729986ae2ce492eac61cec
bob <bob_beck@sil.org>
2024-03-28 12:39:03 -0400

    clean up

--- config/teamcity/package_artifacts.sh
+++ config/teamcity/package_artifacts.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 pwd
-cd ../
 cd ui
 pwd
 /bin/tar cvzf ui.tgz src/
@@ -12,12 +11,8 @@ cp ../tools/DefaultProjectAttributes.java .
 cp ../tools/mongodb_RoleCapability_init.js .
 cp ../tools/mongodb_TestcaseSizes_init.js .
 cp ../tools/mongodb_DefaultProjectAttributes_init.js .
-tar xvzf ui.tgz
 cp ../config/common/startup_dokimion_server.sh .
 cp ../config/common/startup_dokimion_ui.sh .
-chmod -R a+w *
-chmod -R a+w .
-cd src
-cp -r node_modules/tinymce* .
+cp -r src/node_modules/tinymce .
 
 
