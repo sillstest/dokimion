db1733994e65e1feb5efe84950656b4073984a50
Bob Beck <bob_beck@sil.org>
2024-06-18 15:35:41 +0000

    java memory config

--- config/common/startup_dokimion_server.sh
+++ config/common/startup_dokimion_server.sh
@@ -14,7 +14,7 @@ cd /home/dokimion/dokimion
 /usr/bin/javac ConfigurationAttributes.java aes.java AdminPassword.java
 /usr/bin/java ConfigurationAttributes
 
-/usr/bin/java -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/dokimion/dokimion/jetty-runner.jar /home/dokimion/dokimion/dokimion.war | awk -v SERVER_NO=$1 '{printf "dokimion_server_" SERVER_NO; print $0}' | logger -p user.info
+/usr/bin/java -Xmx256m -XX:MetaspaceSize=128m -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/dokimion/dokimion/jetty-runner.jar /home/dokimion/dokimion/dokimion.war | awk -v SERVER_NO=$1 '{printf "dokimion_server_" SERVER_NO; print $0}' | logger -p user.info
 
 
 
