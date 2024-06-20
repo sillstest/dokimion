7ee7459ac535e3af7bd7e811712d42c95522b45f
Bob Beck <bob_beck@sil.org>
2024-06-20 13:38:28 -0400

    removed appmetrics setup

--- config/common/startup_dokimion_ui.sh
+++ config/common/startup_dokimion_ui.sh
@@ -5,7 +5,7 @@
 cd /home/dokimion/dokimion/src
 #export NODE_OPTIONS=--openssl-legacy-provider
 #npm install --legacy-peer-deps
-export NODE_OPTIONS=--openssl-legacy-provider --require appmetrics/start
+export NODE_OPTIONS=--openssl-legacy-provider
 #/usr/bin/npm start 
 pm2 start npm -- start
 
