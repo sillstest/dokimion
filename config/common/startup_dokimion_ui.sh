9102a966faaf6613c6235a12aa8ef778c48462e3
Bob Beck <bob_beck@sil.org>
2024-06-20 13:14:54 -0400

    add pm2 monitoring tool

--- config/common/startup_dokimion_ui.sh
+++ config/common/startup_dokimion_ui.sh
@@ -5,8 +5,9 @@
 cd /home/dokimion/dokimion/src
 #export NODE_OPTIONS=--openssl-legacy-provider
 #npm install --legacy-peer-deps
-export NODE_OPTIONS=--openssl-legacy-provider
-/usr/bin/npm start 
+export NODE_OPTIONS=--openssl-legacy-provider --require appmetrics/start
+#/usr/bin/npm start 
+pm2 start npm -- start
 
 
 
