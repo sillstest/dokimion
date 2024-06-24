62dcdd4e7caa3938acc4c19e0120c60d93438cd8
Bob Beck <bob_beck@sil.org>
2024-06-24 11:22:41 -0400

    twwaked metaspace memory alloc

--- config/common/startup_dokimion_server.sh
+++ config/common/startup_dokimion_server.sh
@@ -11,10 +11,8 @@ cd /home/dokimion/dokimion
 /usr/bin/java DefaultProjectAttributes
 /usr/bin/javac User.java aes.java AdminPassword.java
 /usr/bin/java User
-/usr/bin/javac ConfigurationAttributes.java aes.java AdminPassword.java
-/usr/bin/java ConfigurationAttributes
 
-/usr/bin/java -XX:+UseParallelGC -XX:-UseCompressedOops -Xmx1500m -XX:HeapBaseMinAddress=32g -XX:MetaspaceSize=80m -XX:MaxMetaspaceSize=1500m -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/dokimion/dokimion/jetty-runner.jar /home/dokimion/dokimion/dokimion.war | awk -v SERVER_NO=$1 '{printf "dokimion_server_" SERVER_NO; print $0}' | logger -p user.info
+/usr/bin/java -XX:+UseParallelGC -XX:-UseCompressedOops -Xmx1500m -XX:HeapBaseMinAddress=32g -XX:MetaspaceSize=100m -XX:MaxMetaspaceSize=1500m -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/dokimion/dokimion/jetty-runner.jar /home/dokimion/dokimion/dokimion.war | awk -v SERVER_NO=$1 '{printf "dokimion_server_" SERVER_NO; print $0}' | logger -p user.info
 
 
 
