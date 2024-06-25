38ad39b816ae6a053cc2863161e0db921d667e96
Bob Beck <bob_beck@sil.org>
2024-06-25 18:07:01 +0000

    logging fix

--- config/common/startup_dokimion_ui.sh
+++ config/common/startup_dokimion_ui.sh
@@ -6,7 +6,7 @@ cd /home/dokimion/dokimion/src
 #export NODE_OPTIONS=--openssl-legacy-provider
 #npm install --legacy-peer-deps
 export NODE_OPTIONS=--openssl-legacy-provider
-/usr/bin/npm start 
+/usr/bin/npm start | awk -v UI_NO=$1 '{printf "dokimion_ui_"UI_NO; print $0}'
 
 
 
