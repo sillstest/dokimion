2c9ef15a31609ad7058b97d23f0b5a374af776f7
bob <bob_beck@sil.org>
2024-06-25 10:26:32 -0400

    misc fixes

--- config/common/startup_local_server.sh
+++ config/common/startup_local_server.sh
@@ -4,6 +4,10 @@
 
 cd /home/bob/dokimion
 
-/usr/bin/java -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/bob/dokimion/assembly/target/lib/jetty-runner.jar /home/bob/dokimion/assembly/target/quack.war 
+#/usr/bin/java -Xmx256m -XX:MetaspaceSize=128m -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/bob/dokimion/assembly/target/lib/jetty-runner.jar /home/bob/dokimion/assembly/target/quack.war 
+
+/usr/bin/java -Xmx256m -XX:MetaspaceSize=128m -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/bob/dokimion/assembly/target/lib/jetty-runner.jar /home/bob/dokimion/assembly/target/quack.war | awk -v SERVER_NO=$1 '{printf "dokimion_server_" SERVER_NO; print $0}' | logger -p user.info
+
+
 
 
