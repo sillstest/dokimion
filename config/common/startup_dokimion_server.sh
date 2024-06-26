f1b2fb116aa9eb2bb8688b6e245372394daba624
Bob Beck <bob_beck@sil.org>
2024-06-26 17:12:50 +0000

    add heap dump out of memory option

--- config/common/startup_dokimion_server.sh
+++ config/common/startup_dokimion_server.sh
@@ -12,7 +12,7 @@ cd /home/dokimion/dokimion
 /usr/bin/javac User.java aes.java AdminPassword.java
 /usr/bin/java User
 
-/usr/bin/java -XX:+UseParallelGC -XX:-UseCompressedOops -Xmx1500m -XX:HeapBaseMinAddress=32g -XX:MetaspaceSize=100m -XX:MaxMetaspaceSize=1500m -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/dokimion/dokimion/jetty-runner.jar /home/dokimion/dokimion/dokimion.war | awk -v SERVER_NO=$1 '{printf "dokimion_server_" SERVER_NO; print $0}' | logger -p user.info
+/usr/bin/java -XX:+UseParallelGC -XX:-UseCompressedOops -Xmx1500m -XX:+HeapDumpOnOutOfMemoryError -XX:MetaspaceSize=100m -XX:MaxMetaspaceSize=1500m -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/dokimion/dokimion/jetty-runner.jar /home/dokimion/dokimion/dokimion.war | awk -v SERVER_NO=$1 '{printf "dokimion_server_" SERVER_NO; print $0}' | logger -p user.info
 
 
 
