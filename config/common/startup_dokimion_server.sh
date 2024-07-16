d5625a77cc4a262e86bdc63df851028d834fb2e3
Bob Beck <bob_beck@sil.org>
2024-07-16 08:14:24 -0400

    tweaking memory requirements

--- config/common/startup_dokimion_server.sh
+++ config/common/startup_dokimion_server.sh
@@ -3,7 +3,7 @@
 
 cd /home/dokimion/dokimion
 
-/usr/bin/java  -Dspring.hazelcast.enable=false -XX:+UseParallelGC -XX:-UseCompressedOops -Xmx1500m -XX:+HeapDumpOnOutOfMemoryError -XX:MetaspaceSize=100m -XX:MaxMetaspaceSize=2000m -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/dokimion/dokimion/jetty-runner.jar /home/dokimion/dokimion/dokimion.war | awk -v SERVER_NO=$1 '{printf "dokimion_server_" SERVER_NO; print $0}' | logger -p user.info
+/usr/bin/java  -Dspring.hazelcast.enable=false -XX:+UseParallelGC -XX:-UseCompressedOops -Xmx1500m -XX:+HeapDumpOnOutOfMemoryError -XX:MetaspaceSize=100m -XX:MaxMetaspaceSize=7000m -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/dokimion/dokimion/jetty-runner.jar /home/dokimion/dokimion/dokimion.war | awk -v SERVER_NO=$1 '{printf "dokimion_server_" SERVER_NO; print $0}' | logger -p user.info
 
 
 
