7478fba10c92630a5fc57a9fdbe30752bd2996b8
Bob Beck <bob_beck@sil.org>
2024-09-24 15:06:22 -0400

    replaced hostnames with ip addresses

--- config/production/s-dokimion1/startup_dokimion_server.sh
+++ config/production/s-dokimion1/startup_dokimion_server.sh
@@ -3,7 +3,7 @@
 
 cd /home/dokimion/dokimion
 
-/usr/bin/java -Dcom.management.jmx.remote.host=0.0.0.0 -Dcom.sun.management.jmxremote.registry.ssl=false -Djava.rmi.server.hostname=s-dokimion1.psonet -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=12345 -Dcom.sun.management.jmxremote.rmi.port=12345 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dspring.hazelcast.enable=false -Xss512k -XX:+UseParallelGC -XX:-UseCompressedOops -Xmx1000m -XX:+HeapDumpOnOutOfMemoryError -XX:MetaspaceSize=3000m -XX:MaxMetaspaceSize=11000m -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/dokimion/dokimion/jetty-runner.jar /home/dokimion/dokimion/dokimion.war | awk -v SERVER_NO=$1 '{printf "dokimion_server_" SERVER_NO; print $0}' | logger -p user.info
+/usr/bin/java -Dcom.management.jmx.remote.host=0.0.0.0 -Dcom.sun.management.jmxremote.registry.ssl=false -Djava.rmi.server.hostname=10.3.0.199 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=12345 -Dcom.sun.management.jmxremote.rmi.port=12345 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dspring.hazelcast.enable=false -Xss512k -XX:+UseParallelGC -XX:-UseCompressedOops -Xmx1000m -XX:+HeapDumpOnOutOfMemoryError -XX:MetaspaceSize=3000m -XX:MaxMetaspaceSize=11000m -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/dokimion/dokimion/jetty-runner.jar /home/dokimion/dokimion/dokimion.war | awk -v SERVER_NO=$1 '{printf "dokimion_server_" SERVER_NO; print $0}' | logger -p user.info
 
 
 
