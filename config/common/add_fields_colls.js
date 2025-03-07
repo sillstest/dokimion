46d8e781c8c1f26904246df98b7b881a6b27d844
Bob Beck <bob_beck@sil.org>
2025-03-07 17:05:45 +0000

    new script

---
+++ config/common/add_fields_colls.js
@@ -0,0 +1,16 @@
+
+
+db = db.getSiblingDB('dokimion')
+
+db.helloworld_TestCase.update({}, {$set: {"results": []}},{upsert:false, multi:true})
+db.thescriptu_TestCase.update({}, {$set: {"results": []}},{upsert:false, multi:true})
+db.thecombine_TestCase.update({}, {$set: {"results": []}},{upsert:false, multi:true})
+db.theparatex_TestCase.update({}, {$set: {"results": []}},{upsert:false, multi:true})
+db.silfieldwo_TestCase.update({}, {$set: {"results": []}},{upsert:false, multi:true})
+db.platform.b_TestCase.update({}, {$set: {"results": []}},{upsert:false, multi:true})
+db.paratext2_TestCase.update({}, {$set: {"results": []}},{upsert:false, multi:true})
+db.paratext10_TestCase.update({}, {$set: {"results": []}},{upsert:false, multi:true})
+db.dokimionls_TestCase.update({}, {$set: {"results": []}},{upsert:false, multi:true})
+db.dokimionls_TestCase.update({}, {$set: {"results": []}},{upsert:false, multi:true})
+db.bloomeditor_TestCase.update({}, {$set: {"results": []}},{upsert:false, multi:true})
+db.audioproje_TestCase.update({}, {$set: {"results": []}},{upsert:false, multi:true})
