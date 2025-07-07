605b6710ee86964706de004d76aa6d1e44b693e7
Bob Beck <bob_beck@sil.org>
2025-07-07 14:23:21 +0000

    heap memory tweaking

--- config/production/dokimion1/startup_dokimion_server.sh
+++ config/production/dokimion1/startup_dokimion_server.sh
@@ -3,7 +3,7 @@
 
 cd /home/dokimion/dokimion
 
-/usr/bin/java -Dcom.management.jmx.remote.host=0.0.0.0 -Dcom.sun.management.jmxremote.registry.ssl=false -Djava.rmi.server.hostname=10.3.0.139 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=12345 -Dcom.sun.management.jmxremote.rmi.port=12345 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dspring.hazelcast.enable=false -Xss512k -XX:+UseParallelGC -XX:-UseCompressedOops -Xms500m -Xmx1500m -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:MetaspaceSize=3000m -XX:MaxMetaspaceSize=11000m -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/dokimion/dokimion/jetty-runner.jar /home/dokimion/dokimion/dokimion.war | awk -v SERVER_NO=$1 '{printf "dokimion_server_" SERVER_NO; print $0}' | logger -p user.info
+/usr/bin/java -Dcom.management.jmx.remote.host=0.0.0.0 -Dcom.sun.management.jmxremote.registry.ssl=false -Djava.rmi.server.hostname=10.3.0.139 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=12345 -Dcom.sun.management.jmxremote.rmi.port=12345 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dspring.hazelcast.enable=false -Xss512k -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:NewRatio=2 -verbose:gc -XX:+PrintGCDetails -Xloggc:gc.log -Xms1g -Xmx2g -verbose:gc -XX:+HeapDumpOnOutOfMemoryError -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m -Dmail.debug=true -Xbootclasspath/a:/etc/dokimion -jar /home/dokimion/dokimion/jetty-runner.jar /home/dokimion/dokimion/dokimion.war | awk -v SERVER_NO=$1 '{printf "dokimion_server_" SERVER_NO; print $0}' | logger -p user.info
 
 
 
