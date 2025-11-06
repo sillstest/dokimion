aceac000b173fbd5c1d0a44d75883c4393d16077
Bob Beck <bob_beck@sil.org>
2025-11-06 17:16:31 +0000

    snapshot

--- dal/src/main/java/com/testquack/dal/MongoConfig.java
+++ dal/src/main/java/com/testquack/dal/MongoConfig.java
@@ -76,8 +76,8 @@ System.out.flush();
 
         settingsBuilder.applyToConnectionPoolSettings(builder ->
                        builder.minSize(10)
-                       .maxSize(10)
-                       .maxWaitTime(30, TimeUnit.SECONDS));
+                       .maxSize(300)
+                       .maxWaitTime(60, TimeUnit.SECONDS));
 
         return MongoClients.create(settingsBuilder.build());
     }
