fa2218cb19cb92a329b7402b60f91f5148093f08
Bob Beck <bob_beck@sil.org>
2025-12-19 16:18:34 +0000

    TestSuites.js

--- ui/src/src/testsuites/TestSuites.js
+++ ui/src/src/testsuites/TestSuites.js
@@ -1,164 +1,169 @@
-import React from "react";
-import SubComponent from "../common/SubComponent";
-import { Link } from "react-router-dom";
-import * as Utils from "../common/Utils";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 import ControlledPopup from "../common/ControlledPopup";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinusCircle } from "@fortawesome/free-solid-svg-icons";
 import $ from "jquery";
 import { FadeLoader } from "react-spinners";
 import Backend from "../services/backend";
-import { withRouter } from "../common/withRouter";
 
-class TestSuites extends SubComponent {
-  testSuiteToRemove = null;
+const TestSuites = () => {
+  const { project } = useParams(); // replaces this.props.router.params.project
 
-  state = {
-    testSuites: [],
-    testSuitesToDisplay: [],
-    loading: true,
-    errorMessage: "",
+  const [testSuites, setTestSuites] = useState([]);
+  const [testSuitesToDisplay, setTestSuitesToDisplay] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [testSuiteToRemove, setTestSuiteToRemove] = useState(null);
+
+  useEffect(() => {
+    getTestSuites();
+  }, []); // runs once on mount
+
+  const getTestSuites = () => {
+    Backend.get(`${project}/testsuite`)
+      .then((response) => {
+        setTestSuites(response);
+        setTestSuitesToDisplay(response);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setErrorMessage(`getTestSuites::Couldn't get testsuites, error: ${error}`);
+        setLoading(false);
+      });
   };
 
-  constructor(props) {
-    super(props);
-    this.getTestSuites = this.getTestSuites.bind(this);
-    this.onFilter = this.onFilter.bind(this);
-    this.removeTestSuite = this.removeTestSuite.bind(this);
-    this.removeTestSuiteConfirmation = this.removeTestSuiteConfirmation.bind(this);
-    this.cancelRemoveTestSuiteConfirmation = this.cancelRemoveTestSuiteConfirmation.bind(this);
-  }
-
-  componentDidMount() {
-    super.componentDidMount();
-    this.getTestSuites();
-  }
-
-  getTestSuites() {
-    Backend.get(this.props.router.params.project + "/testsuite")
-      .then(response => {
-        this.state.testSuites = response;
-        this.state.testSuitesToDisplay = this.state.testSuites.slice();
-        this.state.loading = false;
-        this.setState(this.state);
-      })
-      .catch(error => {
-        this.setState({errorMessage: "getTestSuites::Couldn't get testsuites, error: " + error});
-        this.state.loading = false;
-        this.setState(this.state);
-      });
-  }
-
-  onFilter(event) {
-    var token = (event.target.value || "").toLowerCase();
-    this.state.testSuitesToDisplay = this.state.testSuites.filter(testSuite =>
-      (testSuite.name || "").toLowerCase().includes(token),
+  const onFilter = (event) => {
+    const token = (event.target.value || "").toLowerCase();
+    const filtered = testSuites.filter((testSuite) =>
+      (testSuite.name || "").toLowerCase().includes(token)
     );
-    this.setState(this.state);
-  }
+    setTestSuitesToDisplay(filtered);
+  };
 
-  removeTestSuiteConfirmation(testSuiteId) {
-    this.testSuiteToRemove = testSuiteId;
+  const removeTestSuiteConfirmation = (testSuiteId) => {
+    setTestSuiteToRemove(testSuiteId);
     $("#remove-testsuite-confirmation").modal("show");
-  }
+  };
 
-  cancelRemoveTestSuiteConfirmation() {
-    this.testSuiteToRemove = null;
+  const cancelRemoveTestSuiteConfirmation = () => {
+    setTestSuiteToRemove(null);
     $("#remove-testsuite-confirmation").modal("hide");
-  }
+  };
 
-  removeTestSuite(event) {
-    Backend.delete(this.props.router.params.project + "/testsuite/" + this.testSuiteToRemove)
-      .then(response => {
-        // eslint-disable-next-line eqeqeq
-        this.state.testSuites = this.state.testSuites.filter(testSuite => testSuite.id != this.testSuiteToRemove);
-        this.state.testSuitesToDisplay = this.state.testSuites;
-        this.testSuiteToRemove = null;
+  const removeTestSuite = () => {
+    Backend.delete(`${project}/testsuite/${testSuiteToRemove}`)
+      .then(() => {
+        const updatedSuites = testSuites.filter(
+          (testSuite) => testSuite.id !== testSuiteToRemove
+        );
+        setTestSuites(updatedSuites);
+        setTestSuitesToDisplay(updatedSuites);
+        setTestSuiteToRemove(null);
         $("#remove-testsuite-confirmation").modal("hide");
-        this.setState(this.state);
       })
-      .catch(error => {
-        this.setState({errorMessage: "removeTestSuite::Couldn't delete testsuite, error: " + error});
+      .catch((error) => {
+        setErrorMessage(`removeTestSuite::Couldn't delete testsuite, error: ${error}`);
       });
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <ControlledPopup popupMessage={this.state.errorMessage}/>
-        <div className="row">
-          <form className="col-sm-5">
-            <div class="form-group">
-              <input type="text" class="form-control" id="filter" placeholder="Filter" onChange={this.onFilter} />
-            </div>
-          </form>
-        </div>
-        <div className="row">
-          <div className="sweet-loading">
-            <FadeLoader sizeUnit={"px"} size={100} color={"#135f38"} loading={this.state.loading} />
+  return (
+    <div>
+      <ControlledPopup popupMessage={errorMessage} />
+
+      <div className="row">
+        <form className="col-sm-5">
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="filter"
+              placeholder="Filter"
+              onChange={onFilter}
+            />
           </div>
-          {this.state.testSuitesToDisplay.map(
-            function (testSuite) {
-              return (
-                <div className="col-sm-6" key={testSuite.id}>
-                  <div className="card testsuite-card col-sm-10">
-                    <div className="card-body">
-                      <div className="row">
-                        <div className="col-11">
-                          <h5 className="card-title">{testSuite.name}</h5>
-                        </div>
-                        <div className="col1">
-                          <span
-                            className="clickable edit-icon-visible red float-right"
-                            onClick={e => this.removeTestSuiteConfirmation(testSuite.id)}
-                          >
-                            <FontAwesomeIcon icon={faMinusCircle} />
-                          </span>
-                        </div>
-                      </div>
-                      <p className="card-text">
-                        <Link to={"/" + this.props.router.params.project + "/testcases?testSuite=" + testSuite.id}>
-                          View
-                        </Link>
-                      </p>
-                    </div>
-                  </div>
-                  <div className="col-sm-1"></div>
-                </div>
-              );
-            }.bind(this),
-          )}
+        </form>
+      </div>
+
+      <div className="row">
+        <div className="sweet-loading">
+          <FadeLoader sizeUnit="px" size={100} color="#135f38" loading={loading} />
         </div>
 
-        <div className="modal fade" tabIndex="-1" role="dialog" id="remove-testsuite-confirmation">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Remove Test Suite</h5>
-                <button
-                  type="button"
-                  className="close"
-                  onClick={this.cancelRemoveTestSuiteConfirmation}
-                  aria-label="Close"
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
+        {testSuitesToDisplay.map((testSuite) => (
+          <div className="col-sm-6" key={testSuite.id}>
+            <div className="card testsuite-card col-sm-10">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-11">
+                    <h5 className="card-title">{testSuite.name}</h5>
+                  </div>
+                  <div className="col-1">
+                    <span
+                      className="clickable edit-icon-visible red float-right"
+                      onClick={() => removeTestSuiteConfirmation(testSuite.id)}
+                    >
+                      <FontAwesomeIcon icon={faMinusCircle} />
+                    </span>
+                  </div>
+                </div>
+                <p className="card-text">
+                  <Link to={`/${project}/testcases?testSuite=${testSuite.id}`}>
+                    View
+                  </Link>
+                </p>
               </div>
-              <div className="modal-body">Are you sure you want to remove Test Suite?</div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" onClick={this.cancelRemoveTestSuiteConfirmation}>
-                  Close
-                </button>
-                <button type="button" className="btn btn-danger" onClick={this.removeTestSuite}>
-                  Remove Test Suite
-                </button>
-              </div>
+            </div>
+            <div className="col-sm-1" />
+          </div>
+        ))}
+      </div>
+
+      {/* Confirmation Modal */}
+      <div
+        className="modal fade"
+        tabIndex="-1"
+        role="dialog"
+        id="remove-testsuite-confirmation"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Remove Test Suite</h5>
+              <button
+                type="button"
+                className="close"
+                onClick={cancelRemoveTestSuiteConfirmation}
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body">
+              Are you sure you want to remove Test Suite?
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={cancelRemoveTestSuiteConfirmation}
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger"
+                onClick={removeTestSuite}
+              >
+                Remove Test Suite
+              </button>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-export default withRouter(TestSuites);
+// We still wrap with SubComponent (your base class logic) if needed
+export default TestSuites;
--- ui/src/src/testsuites/TestSuitesWidget.js
+++ ui/src/src/testsuites/TestSuitesWidget.js
@@ -1,96 +1,87 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SubComponent from "../common/SubComponent";
 import { Link } from "react-router-dom";
-import * as Utils from "../common/Utils";
 import ControlledPopup from "../common/ControlledPopup";
 import { FadeLoader } from "react-spinners";
 import Backend from "../services/backend";
 
-class TestSuitesWidget extends SubComponent {
-  state = {
-    testSuites: [],
-    testSuitesToDisplay: [],
-    loading: true,
-    errorMessage: "",
+const TestSuitesWidget = ({ projectId, limit = 5 }) => {
+  const [testSuites, setTestSuites] = useState([]);
+  const [testSuitesToDisplay, setTestSuitesToDisplay] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // Fetch test suites whenever projectId changes
+  useEffect(() => {
+    if (!projectId) {
+      setLoading(false);
+      return;
+    }
+
+    const getTestSuites = async () => {
+      setLoading(true);
+      try {
+        const response = await Backend.get(`${projectId}/testsuite`);
+        setTestSuites(response);
+        setTestSuitesToDisplay(response.slice(0, limit));
+        setLoading(false);
+      } catch (error) {
+        setErrorMessage("getTestSuites::Couldn't get testsuites, error: " + error);
+        setLoading(false);
+      }
+    };
+
+    getTestSuites();
+  }, [projectId, limit]);
+
+  const onFilter = (event) => {
+    const token = (event.target.value || "").toLowerCase();
+    const filtered = testSuites
+      .filter((testSuite) =>
+        (testSuite.name || "").toLowerCase().includes(token)
+      )
+      .slice(0, limit);
+    setTestSuitesToDisplay(filtered);
   };
 
-  constructor(props) {
-    super(props);
-    this.limit = props.limit || 5;
-    this.state.projectId = props.projectId;
-    this.getTestSuites = this.getTestSuites.bind(this);
-    this.onFilter = this.onFilter.bind(this);
-  }
+  return (
+    <div>
+      <ControlledPopup popupMessage={errorMessage} />
 
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    var nextProjectId = nextProps.projectId;
-    // eslint-disable-next-line eqeqeq
-    if (nextProjectId && this.state.projectId != nextProjectId) {
-      this.state.projectId = nextProjectId;
-      this.getTestSuites();
-    }
-  }
-
-  componentDidMount() {
-    super.componentDidMount();
-    if (this.state.projectId) {
-      this.getTestSuites();
-    }
-  }
-
-  getTestSuites() {
-    Backend.get(this.state.projectId + "/testsuite")
-      .then(response => {
-        this.state.testSuites = response;
-        this.state.testSuitesToDisplay = this.state.testSuites.slice(0, this.limit);
-        this.state.loading = false;
-        this.setState(this.state);
-      })
-      .catch(error => {
-        this.setState({errorMessage: "getTestSuites::Couldn't get testsuites, error: " + error});
-        this.state.loading = false;
-        this.setState(this.state);
-      });
-  }
-
-  onFilter(event) {
-    var token = (event.target.value || "").toLowerCase();
-    this.state.testSuitesToDisplay = this.state.testSuites
-      .filter(testSuite => (testSuite.name || "").toLowerCase().includes(token))
-      .slice(0, this.limit);
-    this.setState(this.state);
-  }
-
-  render() {
-    return (
-      <div>
-        <ControlledPopup popupMessage={this.state.errorMessage}/>
-        <div className="row">
-          <form className="col-sm-5">
-            <div className="form-group">
-              <input type="text" className="form-control" id="filter" placeholder="Filter" onChange={this.onFilter} />
-            </div>
-          </form>
-        </div>
-        <div>
-          <div className="sweet-loading">
-            <FadeLoader sizeUnit={"px"} size={100} color={"#135f38"} loading={this.state.loading} />
+      <div className="row">
+        <form className="col-sm-5">
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="filter"
+              placeholder="Filter"
+              onChange={onFilter}
+            />
           </div>
-          {this.state.testSuitesToDisplay.map(
-            function (testSuite, index) {
-              return (
-                <div>
-                  <Link key={index} to={"/" + this.state.projectId + "/testcases?testSuite=" + testSuite.id}>
-                    {testSuite.name}
-                  </Link>
-                </div>
-              );
-            }.bind(this),
-          )}
-        </div>
+        </form>
       </div>
-    );
-  }
-}
+
+      <div>
+        <div className="sweet-loading">
+          <FadeLoader
+            sizeUnit={"px"}
+            size={100}
+            color={"#135f38"}
+            loading={loading}
+          />
+        </div>
+
+        {testSuitesToDisplay.map((testSuite, index) => (
+          <div key={testSuite.id || index}>
+            <Link to={`/${projectId}/testcases?testSuite=${testSuite.id}`}>
+              {testSuite.name}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TestSuitesWidget;
