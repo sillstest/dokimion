d82e14350ebe6c0672eadd2adee85fc9948dfa4f
Bob Beck <bob_beck@sil.org>
2026-07-22 14:16:53 -0400

    added 443 for https

--- config/production/dokimion/servers.h
+++ config/production/dokimion/servers.h
@@ -2,11 +2,11 @@ upstream prod_servers {
         ip_hash;
         #least_conn;
         #hash $request_uri;
-        server dokimion1.psonet max_fails=5 fail_timeout=60s;
-        server dokimion2.psonet max_fails=5 fail_timeout=60s;
-        server dokimion3.psonet max_fails=5 fail_timeout=60s;
+        server dokimion1.psonet:443 max_fails=5 fail_timeout=60s;
+        server dokimion2.psonet:443 max_fails=5 fail_timeout=60s;
+        server dokimion3.psonet:443 max_fails=5 fail_timeout=60s;
 
-       # number of keep alive connections to upstream
-       # servers preserved in the cache of each worker process
-       keepalive 64;
+        # number of keep alive connections to upstream
+        # servers preserved in the cache of each worker process
+        keepalive 64;
 }
